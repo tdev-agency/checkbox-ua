7f6462843d613cc71ea61fd22ef9beda9e165bb2
TDev <andrii.trush@gmail.com>
2022-02-02 12:14:30 +0200

    Tmp

--- src/Traits/Createable.php
+++ src/Traits/Createable.php
@@ -7,12 +7,18 @@ trait Createable
 {
     public function __construct(array $data)
     {
+        $this->setData($data);
+    }
+
+    protected function setData($data): self
+    {
         foreach ($data as $key => $value) {
             if (! property_exists(self::class, $key)) {
                 continue;
             }
             $this->$key = $value;
         }
+        return $this;
     }
 
     public static function create(array $data): self
